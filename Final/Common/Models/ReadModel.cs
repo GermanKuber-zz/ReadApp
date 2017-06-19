a156bb2948753e24fafe674b2670cf01e94b412c
Mariano Germán Villarreal Küber <german.kuber@outlook.com>
2017-06-19 18:00:28 -0300

    Se sube App Base.

---
+++ Final/Common/Models/ReadModel.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace Common.Models
+{
+
+    public class CommunityModel
+    {
+        public List<EventModel> Events { get; set; }
+        public string Address { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+        public string Company { get; set; }
+        public string Picture { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int Id { get; set; }
+        public string Link { get; internal set; }
+    }
+}
---
+++ Final/ReadApp/Behavior/AutoScrollBehavior.cs
@@ -0,0 +1,37 @@
+﻿using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Microsoft.Xaml.Interactivity;
+
+namespace ReadApp.Behavior
+{
+
+    [TypeConstraint(typeof(ListViewBase))]
+    public class AutoScrollBehavior : DependencyObject, IBehavior
+    {
+        private ListViewBase AssociatedListView => AssociatedObject as ListView;
+        
+        public DependencyObject AssociatedObject { get; private set; }
+
+        public void Attach(DependencyObject associatedObject)
+        {
+            AssociatedObject = associatedObject;
+
+            if (AssociatedListView != null)
+                AssociatedListView.SelectionChanged += ListViewOnSelectionChanged;
+        }
+
+        public void Detach()
+        {
+            if (AssociatedListView != null)
+                AssociatedListView.SelectionChanged -= ListViewOnSelectionChanged;
+        }
+
+        private void ListViewOnSelectionChanged(object sender, SelectionChangedEventArgs selectionChangedEventArgs)
+        {
+            if (AssociatedListView.SelectedItem == null)
+                return;
+
+            AssociatedListView.ScrollIntoView(AssociatedListView.SelectedItem);
+        }
+    }
+}
